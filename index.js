7724a3c999c268497ed14d7245a7285aa5649b31
celcoded <celcrls.ca@gmail.com>
2025-12-15 15:04:46 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+
+const mainContainerElement = document.getElementById('main-container');
+const projectContainerElement = document.getElementById('project-container');
+const projectInnerContainerElement = document.getElementById('project-inner-container');
+const subTextElement = document.getElementById('subText');
+const cursorElement = document.getElementById('cursor');
+
+typeAnimation();
+mainContainerElement.addEventListener('mousemove', (e) => {
+    mouseMoved(e);
+})
+document.addEventListener('mouseup', (e) => {
+    cursorElement.style.transform = "unset";
+    cursorElement.style.display = "block";
+})
+document.addEventListener('mousedown', (e) => {
+    cursorElement.style.transform = "scale(.8)"
+})
+projectContainerElement.addEventListener('mousedown', (e) => {
+    cursorElement.style.display = "none"
+})
+projectInnerContainerElement.addEventListener('mousedown', (e) => {
+    e.stopPropagation();
+    cursorElement.style.transform = "scale(.8)"
+    cursorElement.style.display = "block";
+})
+
+function mouseMoved(event) {
+    const cursorElement = document.getElementById('cursor');
+    const rect = mainContainerElement.getBoundingClientRect();
+
+    const centerX = rect.left + rect.width / 2;
+    const centerY = rect.top + rect.height / 2;
+
+    const offsetX = event.clientX - centerX;
+    const offsetY = event.clientY - centerY;
+
+    const moveX = offsetX / 35;
+    const moveY = offsetY / 35;
+
+    mainContainerElement.style.backgroundPosition = `${50 + moveX}% ${50 + moveY}%`;
+    cursorElement.style.top = `${event.clientY}px`;
+    cursorElement.style.left = `${event.clientX}px`;
+}
+
+function typeAnimation() {
+    const text = "Let's collaborate to create applications worthy of being published.";
+    if (text) {
+    const characters = text.split("");
+    characters.forEach((character, index) => {
+        setTimeout(() => {
+            subTextElement.textContent = subTextElement.textContent.concat(character);
+        }, 50 * index);
+    });
+    }
+}
